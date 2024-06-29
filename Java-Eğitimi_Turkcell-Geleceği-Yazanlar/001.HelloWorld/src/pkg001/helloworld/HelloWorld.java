aaa91fd48f7f85aec4c8c61d6bf8d3fc8671eb74
enginbas96 <engin.bas96@gmail.com>
2024-06-29 16:41:33 +0300

    First App
    Hello World!

---
+++ Java-Eğitimi_Turkcell-Geleceği-Yazanlar/001.HelloWorld/src/pkg001/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package pkg001.helloworld;
+
+public class HelloWorld 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");
+        System.out.println("Im new here.");
+        System.out.println("I wanna learn Java language.");
+    }    
+}
