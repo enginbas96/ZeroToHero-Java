7507745fa2e4bbfa446baab4c880b02503577e2f
enginbas96 <engin.bas96@gmail.com>
2024-07-02 20:48:41 +0300

    Logical Operators Examples

---
+++ Java-Eğitimi_Turkcell-Geleceği-Yazanlar/022.LogicalOperatorsExamples/src/pkg022/logicaloperatorsexamples/LogicalOperatorsExamples.java
@@ -0,0 +1,17 @@
+package pkg022.logicaloperatorsexamples;
+
+public class LogicalOperatorsExamples 
+{
+
+    public static void main(String[] args)
+    {
+        boolean a = false;
+        boolean b = true;
+        
+        System.out.println(a && b); 
+        System.out.println(a || b);
+        System.out.println(!a);
+        System.out.println(!a || b);
+        System.out.println(!a && b);      
+    }    
+}
