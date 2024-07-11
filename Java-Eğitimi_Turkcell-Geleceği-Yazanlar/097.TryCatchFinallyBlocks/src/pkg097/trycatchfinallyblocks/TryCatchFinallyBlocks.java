fd51e71e258319dc92b5d2b68eb7a0f59e66f5dc
enginbas96 <engin.bas96@gmail.com>
2024-07-11 14:37:31 +0300

    Revert "TryCatchFinallyExamples"
    This reverts commit 2ce3decd13754f7fc7aff904428da5691209a653.

--- Java-Eğitimi_Turkcell-Geleceği-Yazanlar/097.TryCatchFinallyBlocks/src/pkg097/trycatchfinallyblocks/TryCatchFinallyBlocks.java
+++ Java-Eğitimi_Turkcell-Geleceği-Yazanlar/097.TryCatchFinallyBlocks/src/pkg097/trycatchfinallyblocks/TryCatchFinallyBlocks.java
@@ -13,7 +13,8 @@ public class TryCatchFinallyBlocks
         
         //Bazı durumlarda if else blokları try catch bloklarından daha performanslı çalışır bu sebeple ifelse blokları tercih edilir
         if(index < numbers.length && index >= 0){System.out.println(numbers[index]);}
-        else{System.out.println("Yanlis index numarasi.");}        
+        else{System.out.println("Yanlis index numarasi.");}
+        
         /* //Sürekli try catch kullanmak bazen istenmeyen bir durum olabilir 
         try{System.out.println(numbers[index]);} 
         catch (Exception e){System.out.println("Yanlis index numarasi.");}
