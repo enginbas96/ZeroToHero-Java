882ec7e6f47090b75adffd1bc2597e958abfcd97
enginbas96 <engin.bas96@gmail.com>
2024-07-11 14:40:25 +0300

    TryCatchFinallyBlocks-2

--- Java-Eğitimi_Turkcell-Geleceği-Yazanlar/097.TryCatchFinallyBlocks/src/pkg097/trycatchfinallyblocks/TryCatchFinallyBlocks.java
+++ Java-Eğitimi_Turkcell-Geleceği-Yazanlar/097.TryCatchFinallyBlocks/src/pkg097/trycatchfinallyblocks/TryCatchFinallyBlocks.java
@@ -10,11 +10,11 @@ public class TryCatchFinallyBlocks
         Scanner sc = new Scanner(System.in);
         System.out.print("Index degerini giriniz : ");
         int index = sc.nextInt();
-        
+
         //Bazı durumlarda if else blokları try catch bloklarından daha performanslı çalışır bu sebeple ifelse blokları tercih edilir
         if(index < numbers.length && index >= 0){System.out.println(numbers[index]);}
         else{System.out.println("Yanlis index numarasi.");}
-        
+
         /* //Sürekli try catch kullanmak bazen istenmeyen bir durum olabilir 
         try{System.out.println(numbers[index]);} 
         catch (Exception e){System.out.println("Yanlis index numarasi.");}
