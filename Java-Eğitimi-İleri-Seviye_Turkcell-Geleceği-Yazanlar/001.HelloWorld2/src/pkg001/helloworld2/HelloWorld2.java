12282f8e5bde092f393cd00fc871901c73fc56b4
enginbas96 <engin.bas96@gmail.com>
2024-07-28 21:38:40 +0300

    HelloWorld-2

---
+++ Java-Eğitimi-İleri-Seviye_Turkcell-Geleceği-Yazanlar/001.HelloWorld2/src/pkg001/helloworld2/HelloWorld2.java
@@ -0,0 +1,10 @@
+package pkg001.helloworld2;
+
+public class HelloWorld2 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World 2");
+        System.out.println("I wanna learn Java language.");
+    }    
+}
