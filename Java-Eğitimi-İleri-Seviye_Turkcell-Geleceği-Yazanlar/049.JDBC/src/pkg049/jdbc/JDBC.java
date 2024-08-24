59a43494617580f74e31407a174b6838480f74fa
enginbas96 <engin.bas96@gmail.com>
2024-08-24 18:42:52 +0300

    JDBC-1

---
+++ Java-Eğitimi-İleri-Seviye_Turkcell-Geleceği-Yazanlar/049.JDBC/src/pkg049/jdbc/JDBC.java
@@ -0,0 +1,19 @@
+package pkg049.jdbc;
+
+import java.sql.*;
+
+//Java DataBase Connectivity
+public class JDBC 
+{
+    public static void main(String[] args) 
+    {
+        //Yapılacaklar;
+        //1.İmport java.sql
+        //2. load and register the driver
+        //3. create connection; veritabanı baglantısı
+        //4. create a statement; sorguları hazırlama
+        //5. execute the query; sorguları çalıştırma
+        //6. process the results; sonuçları işleme almak
+        //7. close connection; bağlantıyı kapatmak
+    }    
+}
