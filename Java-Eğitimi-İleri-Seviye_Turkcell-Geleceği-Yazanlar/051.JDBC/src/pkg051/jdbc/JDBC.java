85f63b16ca776dd9084fa9e8d48985ac0c8afc84
enginbas96 <engin.bas96@gmail.com>
2024-08-29 16:28:51 +0300

    JDBC-3

--- Java-Eğitimi-İleri-Seviye_Turkcell-Geleceği-Yazanlar/051.JDBC/src/pkg051/jdbc/JDBC.java
+++ Java-Eğitimi-İleri-Seviye_Turkcell-Geleceği-Yazanlar/051.JDBC/src/pkg051/jdbc/JDBC.java
@@ -1,9 +1,37 @@
 package pkg051.jdbc;
 
+import java.sql.*;
+
 public class JDBC 
 {
-    public static void main(String[] args) 
+    public static void main(String[] args) throws SQLException 
     {
-        //OFFDAY 2/2
-    }        
+        //Manuel veri girişi yapıldı
+        
+        String url = "jdbc:mysql://localhost:3306/okul";
+        String username = "root";
+        String password = "";       
+
+        Connection conn = null;  
+        conn = DriverManager.getConnection(url, username, password); 
+
+        String sorgu = "INSERT INTO ogrenciler (ogrenciNo, username, ad, soyad) VALUES (?, ?, ?, ?)";    
+        
+        PreparedStatement pst = conn.prepareStatement(sorgu);
+        
+        int ogrenciNo = 1; // Örneğin, öğrenci numarası 1
+        String ogrenciUsername = "ahmet123";
+        String ogrenciAd = "Ahmet";
+        String ogrenciSoyad = "Yılmaz";
+        
+        pst.setInt(1, ogrenciNo);
+        pst.setString(2, ogrenciUsername);
+        pst.setString(3, ogrenciAd);
+        pst.setString(4, ogrenciSoyad);
+        
+        pst.executeUpdate();
+         
+        pst.close();
+        conn.close(); 
+    }    
 }
