fd1a4cf1a5eadcc59146b5dd4b849b7929309069
enginbas96 <engin.bas96@gmail.com>
2024-09-01 18:53:58 +0300

    JDBC-5

--- Java-Eğitimi-İleri-Seviye_Turkcell-Geleceği-Yazanlar/053.JDBC/src/pkg053/jdbc/JDBC.java
+++ Java-Eğitimi-İleri-Seviye_Turkcell-Geleceği-Yazanlar/053.JDBC/src/pkg053/jdbc/JDBC.java
@@ -1,20 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package pkg053.jdbc;
 
-/**
- *
- * @author ENGBA
- */
-public class JDBC {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+public class JDBC 
+{
+    public static void main(String[] args) 
+    {
+        //Eğitim videolarının kaynağı kaldırılmış
+        // Aynı konulara paralel yeni eğitim videoları araştırılarak eğitim tamamlanmaya çalışılacaktır
+        //offDay        
+    }    
 }
