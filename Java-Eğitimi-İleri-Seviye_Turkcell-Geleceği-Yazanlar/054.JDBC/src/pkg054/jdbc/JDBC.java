29e427ca433787eecb42ca3ba17a402823b40738
enginbas96 <engin.bas96@gmail.com>
2024-09-04 20:33:39 +0300

    JDBC-6

---
+++ Java-Eğitimi-İleri-Seviye_Turkcell-Geleceği-Yazanlar/054.JDBC/src/pkg054/jdbc/JDBC.java
@@ -0,0 +1,24 @@
+package pkg054.jdbc;
+
+import java.sql.SQLException;
+
+public class JDBC 
+{
+    public static void main(String[] args) throws SQLException 
+    {
+        OgrenciDataAccessObject ODAO = new OgrenciDataAccessObject();
+        Ogrenci ogrenci = new Ogrenci();
+        
+        ogrenci.setNo("152356");
+        ogrenci.setkAdi("MahTum123");
+        ogrenci.setAd("Mahmut");
+        ogrenci.setSoyad("Tutam");        
+        ogrenci.bilgiVer();
+        ODAO.ogrenciEkle(ogrenci);        
+        
+        System.out.println("----------------------------------------");        
+        
+        String numara = "234234";  
+        ODAO.getOgrenci(numara).bilgiVer();
+    }    
+}
