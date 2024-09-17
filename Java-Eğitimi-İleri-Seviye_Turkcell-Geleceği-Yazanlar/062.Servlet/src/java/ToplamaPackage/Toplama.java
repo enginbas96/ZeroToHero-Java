ae35443641f5bd72609e54bf380ba75079d86550
enginbas96 <engin.bas96@gmail.com>
2024-09-17 16:21:08 +0300

    Servlet-5

--- Java-Eğitimi-İleri-Seviye_Turkcell-Geleceği-Yazanlar/062.Servlet/src/java/ToplamaPackage/Toplama.java
+++ Java-Eğitimi-İleri-Seviye_Turkcell-Geleceği-Yazanlar/062.Servlet/src/java/ToplamaPackage/Toplama.java
@@ -20,51 +20,13 @@ public class Toplama extends HttpServlet
             resp.setCharacterEncoding("UTF-8");
             resp.setHeader("content-type", "text/html;charset=UTF-8"); 
             
-            List<String> takimlarList = new ArrayList<>()
-            {
-                {
-                    add("Galatasaray");
-                    add("Fenerbahçe");
-                    add("Beşiktaş");
-                    add("Trabzonspor");
-                    //Buraya istenilen takım eklenildiğinde tablo otomatik olarak güncellenecek                    
-                    add("Beykozspor");
-                }
-            };
+            int sayi1 = Integer.parseInt(req.getParameter("sayi1"));
+            int sayi2 = Integer.parseInt(req.getParameter("sayi2"));
+            int sonuc = sayi1 + sayi2;            
+            writer.println("<p>Sonuç:" + sonuc + "</p>");          
             
-            ArrayList<String> renkler = new ArrayList<>()
-            {
-                {
-                    add("red");
-                    add("blue");
-                    add("green");
-                    add("yellow");
-                }                
-            };
-            
-            String[] stilRenkleri = new String[renkler.size()];
-            stilRenkleri = renkler.toArray(stilRenkleri);
-            //String[] stilRenkleri = renkler.toArray(new String[0]);            
-            int index = 0;
-            
-            writer.write("<table>");
-            
-            writer.write("<th>");
-            writer.write("TAKIMLAR");
-            writer.write("</th>");           
-            
-            for(String takim:takimlarList)
-            {
-                writer.write("<tr>");
-                writer.write("<td style='color:"+stilRenkleri[index++ % stilRenkleri.length]+"'>");
-                //writer.write("<td style='color:"+stilRenkleri[index++]+"'>"); //Problemler çıkabilir renk ve takım sayılarının birbirine eşit olması lazım
-                
-                writer.write(takim);
-                
-                writer.write("</td>");                
-                writer.write("</tr>");
-            } 
-            
-            writer.write("</table>");            
+            //Get metodunu kullandığımız için tarayıcı barında tüm değerler şeffaf bir şekilde gözükecektir
+            //Fakat biz bir şifre vs. alıyor olsaydık görünmesini istemeyecektik
+            //Bunun için get metodu yerine post metodunu kullanmamız gerekecekti
         }
 }
