f39a8ddb4a1d85594a59def29bcfff870c1bf844
enginbas96 <engin.bas96@gmail.com>
2024-10-18 21:05:17 +0300

    012.MultiArrays

---
+++ Java-Eğitimi-2-Başlangıç-Orta-Seviye_BTK-Akademi/012.MultiArrays/src/Main.java
@@ -0,0 +1,24 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String[][] matris = new String[3][3];
+        for(int i = 0; i<3; i++)
+        {
+            for(int j = 0; j<3; j++)
+            {
+                matris[i][j] = i + ". satır, " + j + ". stun   |";
+            }
+        }
+        System.out.println("---------------------------------------------------------------------");
+        for(int i = 0; i<3; i++)
+        {
+            for(int j = 0; j<3; j++)
+            {
+                System.out.print(matris[i][j] + "   ");
+            }
+            System.out.println();
+            System.out.println("---------------------------------------------------------------------");
+        }
+    }
+}
