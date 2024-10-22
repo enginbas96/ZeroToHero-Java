1042800f639894b6af2df516445eb893b697b4b1
enginbas96 <engin.bas96@gmail.com>
2024-10-22 19:48:51 +0300

    020.Methods-1

--- Java-Eğitimi-2-Başlangıç-Orta-Seviye_BTK-Akademi/020.Methods/src/Main.java
+++ Java-Eğitimi-2-Başlangıç-Orta-Seviye_BTK-Akademi/020.Methods/src/Main.java
@@ -1,6 +1,39 @@
-public class Main {
+public class Main
+{
     public static void main(String[] args)
     {
-        //offday
+        //Metotlar kod tekrarından kurtarır
+        //Metotların içerine yazılan kodu istenilen yerde metodu çağırarak çalıştırılabilir
+
+        ParametresizMesajVer();
+        ParametreliMesajVer("Engin BAŞ");
+
+        int[] sayilar = new int[] {1,5,3,2,78,213,4};
+        SayiBulmaca(sayilar,5);
+
+
+    }
+
+    public static void ParametresizMesajVer()
+    {
+        System.out.println("Bu metot parametresiz metottur.");
+    }
+
+    public static void ParametreliMesajVer(String parametre)
+    {
+        System.out.println("Bu metot parametreli metottur. Parametre = " + parametre);
+    }
+
+    public static void SayiBulmaca(int[] sayiDizisi, int aranacakSayi)
+    {
+        for(int i:sayiDizisi)
+        {
+            if(i == aranacakSayi)
+            {
+                System.out.println("Aradığınız sayı dizide mevcuttur.");
+                return;
+            }
+        }
+        System.out.println("Aranacak sayı dizede bulunamadı.");
     }
 }
