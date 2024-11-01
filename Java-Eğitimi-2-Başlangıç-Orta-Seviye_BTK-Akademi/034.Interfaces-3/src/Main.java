4bfa503b9a292eae3b6d9d2822ba8a79d9f8c900
enginbas96 <engin.bas96@gmail.com>
2024-11-01 20:01:41 +0300

    034.Interfaces-3

---
+++ Java-Eğitimi-2-Başlangıç-Orta-Seviye_BTK-Akademi/034.Interfaces-3/src/Main.java
@@ -0,0 +1,7 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //Sınıfların ve İnterfacelerin arasında gezinerek anlayabiliriz.
+    }
+}
---
+++ Java-Eğitimi-2-Başlangıç-Orta-Seviye_BTK-Akademi/034.Interfaces-3/src/Worker.java
@@ -0,0 +1,17 @@
+public class Worker implements IWorkable,IEatable, IPayable
+{
+    @Override
+    public void Work() {
+
+    }
+
+    @Override
+    public void Eat() {
+
+    }
+
+    @Override
+    public void Pay() {
+
+    }
+}
