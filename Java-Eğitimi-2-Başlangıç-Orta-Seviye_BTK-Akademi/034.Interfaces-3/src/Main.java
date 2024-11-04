0c635a54761f077fcba7708d8aa4a220cf1cd11c
enginbas96 <93674090+enginbas96@users.noreply.github.com>
2024-11-04 20:54:19 +0300

    034.Interfaces-3

--- Java-Eğitimi-2-Başlangıç-Orta-Seviye_BTK-Akademi/034.Interfaces-3/src/Main.java
+++ Java-Eğitimi-2-Başlangıç-Orta-Seviye_BTK-Akademi/034.Interfaces-3/src/Main.java
@@ -2,6 +2,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        //Sınıfların ve İnterfacelerin arasında gezinerek anlayabiliriz.
+        //Sınıfların ve İnterfacelerin arasında gezinerek anlayabiliriz
     }
 }
