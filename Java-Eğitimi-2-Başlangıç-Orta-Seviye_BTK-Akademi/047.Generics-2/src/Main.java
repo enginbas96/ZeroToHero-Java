d7c51c6398944b9071fb698e5dc33de67919ba31
enginbas96 <engin.bas96@gmail.com>
2025-07-20 22:13:15 +0300

    047.Generics-2

---
+++ Java-Eğitimi-2-Başlangıç-Orta-Seviye_BTK-Akademi/047.Generics-2/src/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        
+        Customer customer = new Customer();
+        Validator validator = new Validator();
+
+        validator.Validate(customer);
+    }
+}
---
+++ Java-Eğitimi-2-Başlangıç-Orta-Seviye_BTK-Akademi/047.Generics-2/src/Validator.java
@@ -0,0 +1,8 @@
+public class Validator
+{
+    //Generic metot örneği
+    public <T extends IEntity> void Validate(T deger)
+    {
+
+    }
+}
